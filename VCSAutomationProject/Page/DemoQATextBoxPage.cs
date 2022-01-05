223b6292e42d9eef91be7a78dade63d9c81f2da7
MSI\giedr <giedrius.pavalkis@gmail.com>
2022-01-05 21:03:32 +0200

    added Page and Tests folders with text-box test

---
+++ VCSAutomationProject/Page/DemoQATextBoxPage.cs
@@ -0,0 +1,53 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VCSAutomationProject.Page
+{
+    class DemoQATextBoxPage
+    {
+        private static IWebDriver _driver;
+        
+        private IWebElement _fullNameInputField => _driver.FindElement(By.Id("userName"));
+        private IWebElement _submitButton => _driver.FindElement(By.Id("submit"));
+        private IWebElement _fullNameActualResult => _driver.FindElement(By.Id("name"));
+
+        public DemoQATextBoxPage(IWebDriver webdriver)
+        {
+            _driver = webdriver;
+        }
+
+        public void InsertTextFullNameField(string text)
+        {
+            _fullNameInputField.Clear();
+            _fullNameInputField.SendKeys(text);
+        }
+
+        public void ClickSubmitButton()
+        {
+            _submitButton.Click();
+        }
+
+        public void VerifyFullNameResult(string expectedResult)
+        {
+            Assert.AreEqual($"Name:{expectedResult}", _fullNameActualResult.Text, "Name is wrong");
+        }
+
+        //IWebDriver driver = new ChromeDriver();
+        //driver.Url = "https://demoqa.com/text-box";
+
+        //    IWebElement fullNameInputField = driver.FindElement(By.Id("userName"));
+        //fullNameInputField.SendKeys("Giedrius Pavalkis");
+
+        //    IWebElement submitButton = driver.FindElement(By.Id("submit"));
+        //submitButton.Click();
+
+        //    IWebElement newName = driver.FindElement(By.Id("name"));
+        //Assert.AreEqual("Name:Giedrius Pavalkis", newName.Text, "Name is wrong");
+    }
+}
---
+++ VCSAutomationProject/Tests/DemoQATextBoxTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VCSAutomationProject.Page;
+
+namespace VCSAutomationProject.Tests
+{
+    class DemoQATextBoxTest
+    {
+
+        private static IWebDriver _driver; 
+
+        [OneTimeSetUp]
+
+        public void SetUp()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Manage().Window.Maximize();
+            _driver.Url = "https://demoqa.com/text-box";
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            _driver.Close();
+        }
+
+        [Test]
+        public static void FullNameTextBoxTest()
+        {
+            DemoQATextBoxPage demoQATextBoxPage = new DemoQATextBoxPage(_driver);
+
+            string text = "Giedrius";
+
+            demoQATextBoxPage.InsertTextFullNameField(text);
+            demoQATextBoxPage.ClickSubmitButton();
+            demoQATextBoxPage.VerifyFullNameResult(text);
+        }
+    }
+}
